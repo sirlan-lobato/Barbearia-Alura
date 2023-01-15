bc5ac4e9ee7ed7afb96b0407395da3247b4bfea5
Sirlan Lobato <115949546+sirlan-lobato@users.noreply.github.com>
2023-01-15 10:17:39 -0300

    Inserindo Arquivos

---
+++ assets/function.js
@@ -0,0 +1,10 @@
+
+
+
+const botaoMenu = document.getElementById('botao');
+const menu = document.getElementById('menu-lateral');
+
+    botaoMenu.addEventListener('click', ()=>{
+        menu.classList.toggle('menu__navegacao-ativo')
+    });
+
